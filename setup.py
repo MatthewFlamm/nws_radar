902345270b93ef20d38ed1b6522d7cbc790dfd7e
ktdad <matthewhflamm@gmail.com>
2019-05-28 08:18:06 -0400

    version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "nws_radar",
-    version = "0.0.3",
+    version = "0.0.4",
     license='MIT License',
     long_description=long_description,
     long_description_content_type="text/markdown",
